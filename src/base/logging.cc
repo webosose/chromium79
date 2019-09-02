7db27d35b92a96392c30af2be057bd0c6ada6968
Jose Dapena Paz <jose.dapena@lge.com>
2019-09-02 16:42:28 +0200

    [op][n_upsable][logging][webos] Do not dump stacktrace on fatal errors
    :Release Notes:
    Do not dump stacktrace on fatal errors.
    
    :Detailed Notes:
    webOS provides other ways to obtain the crash stacktrace, so it is
    redundant to also dump it to stderr on fatal errors.
    
    :Testing Performed:
    1. Tail std error of WAM
    2. Restart webapp-mgr, check FATAL error generated for failing sandbox does not
    dump a stacktrace.
    
    :QA Notes:
    Test-cases: GT-27829
    
    :Issues Addressed:
    [PLAT-98724] Port patch: [open][neva_upstreamable][logging][webos] Do not dump stacktrace on fatal errors
    
    Change-Id: I343aa6b845dd7b2965c4142fd275c0177e3a0eaf
    Originally-Reviewed-on: http://gpro.lge.com/254024
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium79/+/266914
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/base/logging.cc
+++ src/base/logging.cc
@@ -623,7 +623,7 @@ LogMessage::LogMessage(const char* file, int line, LogSeverity severity,
 LogMessage::~LogMessage() {
   size_t stack_start = stream_.tellp();
 #if !defined(OFFICIAL_BUILD) && !defined(OS_NACL) && !defined(__UCLIBC__) && \
-    !defined(OS_AIX)
+    !defined(OS_AIX) && !defined(OS_WEBOS)
   if (severity_ == LOG_FATAL && !base::debug::BeingDebugged()) {
     // Include a stack trace on a fatal, unless a debugger is attached.
     base::debug::StackTrace stack_trace;
