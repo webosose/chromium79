e254bdda8cee9e1fdc86a5f4f3c7d992d6621594
Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
2019-12-29 08:25:59 -0800

    [op][ups][build][base] Update revision of upstream libjpeg_turbo (adapt webos build v.79)
    :Release Notes:
    Update revision used for libjpeg_turbo in neva-1.2-release
    This version change was done to adapt v79 to webos build
    
    :Detailed Notes:
    Partial change from: neva/chromium
        commit: 4396f3dea477c274486d76115bfd1912862fe2a9
        Author: Vlad Mukulov <vladislav.mukulov@lge.com>
        Date: Nov 17, 2019
    
        [build][webos] Adapt webos build to chromium v.79
    
    Change libjpeg_turbo to 61a2bbaa9aec from 9d3bf3e9680 (orig v79)
    
    :Testing Performed:
    Confirm it doesnt effect size
    
    :QA Notes:
    Test-cases: N/A
    
    :Issues Addressed:
    [PLAT-98156] Create RP fork of neva 1.2 release
    
    Change-Id: I02111f103d310fb7cc4d7db7407d8485cbbbfebb

--- src/third_party/libjpeg_turbo/tjutil.c
+++ src/third_party/libjpeg_turbo/tjutil.c
@@ -30,26 +30,25 @@
 
 #include <windows.h>
 
-static double getFreq(void)
+static double getfreq(void)
 {
-  LARGE_INTEGER freq;
-
-  if (!QueryPerformanceFrequency(&freq)) return 0.0;
-  return (double)freq.QuadPart;
+	LARGE_INTEGER freq;
+	if(!QueryPerformanceFrequency(&freq)) return 0.0;
+	return (double)freq.QuadPart;
 }
 
-static double f = -1.0;
+static double f=-1.0;
 
-double getTime(void)
+double gettime(void)
 {
-  LARGE_INTEGER t;
-
-  if (f < 0.0) f = getFreq();
-  if (f == 0.0) return (double)GetTickCount() / 1000.;
-  else {
-    QueryPerformanceCounter(&t);
-    return (double)t.QuadPart / f;
-  }
+	LARGE_INTEGER t;
+	if(f<0.0) f=getfreq();
+	if(f==0.0) return (double)GetTickCount()/1000.;
+	else
+	{
+		QueryPerformanceCounter(&t);
+		return (double)t.QuadPart/f;
+	}
 }
 
 #else
@@ -57,12 +56,11 @@ double getTime(void)
 #include <stdlib.h>
 #include <sys/time.h>
 
-double getTime(void)
+double gettime(void)
 {
-  struct timeval tv;
-
-  if (gettimeofday(&tv, NULL) < 0) return 0.0;
-  else return (double)tv.tv_sec + ((double)tv.tv_usec / 1000000.);
+	struct timeval tv;
+	if(gettimeofday(&tv, NULL)<0) return 0.0;
+	else return (double)tv.tv_sec+((double)tv.tv_usec/1000000.);
 }
 
 #endif
