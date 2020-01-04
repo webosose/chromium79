170c2271375d8ad8f1866939daabe588135d18ed
Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
2020-01-04 09:28:24 -0800

    fixup! [op][ds][build][scripts] Helper scripts for builds and developers
    :Release Notes:
    fix the license check script
    
    :Detailed Notes:
    fix the license check script
    
    :Testing Performed:
    check the files for proper license header
    
    :QA Notes:
    Test-cases: N/A
    
    :Issues Addressed:
    [PLAT-98297] Check license and add README
    
    Change-Id: Idfea2062f3a11cbb42582d781873dcb7d0dca27f
    Reviewed-on: http://gpro.lge.com/266514
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium79/+/266903
    Reviewed-by: OSE Helper Bot <ose@lge.com>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- scripts/build/list-suspect-files-lg-license.sh
+++ scripts/build/list-suspect-files-lg-license.sh
@@ -3,9 +3,9 @@
 # This script are steps on how to search for files with
 # LG Copyright and BSD-style license
 
-git grep -i "Copyright" * | grep LG > all-files-with-lg-license
+git grep -i "Copyright" * | grep -l LG > all-files-with-lg-license
 for f in `cat all-files-with-lg-license`; do
-	grep -L "BSD-style" $f;
+	grep -l "BSD-style" $f;
 done
 
 # Above should print files with an issue
