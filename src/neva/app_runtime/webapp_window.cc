0d456963a7fd66ee829a68864bfe4ae935e9a5b1
Minju Kim <minju2.kim@lge.com>
2020-01-14 14:04:03 +0900

    [op][n_upsable][keep_alive] Allow recreating host and widget when webview is created already
    :Release Notes:
    Allow recreating host and widget when webview and web_contents are created
    already from the crash of renderer process
    
    :Detailed Notes:
    When renderer process crashed while the window of "keep alive" web app is hidden,
    webview and web_contents are created by recreateWebView
    before recreating host and widget.
    So host and wiget has to be recreated when webview already exists.
    
    :Testing Performed:
    1. Launch com.webos.app.home
    2. Touch the trasparent area to hide the window of web app
    3. Check the running list and kill $webprocessid of com.webos.app.home
    4. Relaunch com.webos.app.home
    
    :QA Notes:
    Depends on: http://gpro.lge.com/268304 (WAM)
    Test-cases: QWA-2129
    
    :Issues Addressed:
    [PLAT-98162] HomeApp is not launched and sometimes click is
    not working.
    
    Change-Id: I1c35924a708aed66cacb74ea391fabea36c35335
    Originally-Reviewed-on: http://gpro.lge.com/268305
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium79/+/268494
    Reviewed-by: OSE Helper Bot <ose@lge.com>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/neva/app_runtime/webapp_window.cc
+++ src/neva/app_runtime/webapp_window.cc
@@ -758,7 +758,7 @@ void WebAppWindow::InitWindow() {
 }
 
 void WebAppWindow::RecreateIfNeeded() {
-  if (widget_ || host_ || webview_ || !GetWebContents())
+  if (widget_ || host_ || !GetWebContents())
     return;
 
   SetupWebContents(GetWebContents());
