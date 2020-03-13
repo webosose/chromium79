58a508ac581bde7d12fae901a5b3dbfe21c4531d
Jose Dapena Paz <jose.dapena@lge.com>
2020-03-13 20:20:59 +0100

    [op][upsable][a11y] Allow to make groups of checkboxes
    :Release Notes:
    Consider groups of checkboxes as valid ARIA sets.
    
    :Detailed Notes:
    ARIA does not expect any container with items inside to be a set. It
    specifies certain rules for what can be a set, and what kind of items
    can be their members. That does not include a "group" of "checkboxes".
    
    This is a bit inconsistent, as on the other hand a menu checkbox is
    considered as a valid item inside a menu bar or menu. This change
    allows checkbox to be a valid item (a member of a set), and allows
    it to be a member of a group set.
    
    :Testing Performed:
    1. Open enyo2sampler
    2. Go to Moonstone/Checkbox item case
    3. Focus an item on the checkbox item group panel
    TTS should say the name of the member, if it is checked or not, and
    the index and set size (i.e. "Group Option 1, unchecked, 1 of 5").
    
    :QA Notes:
    Test-cases: GT-22314
    
    :Issues Addressed:
    [PLAT-102530] [A11Y] Check box item group audio guidance does not add count suffix
    
    Change-Id: I25a06b9f1d4297280af807a3ac175544f2c807e8
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium79/+/271259
    Reviewed-by: Build_Verification
    Reviewed-by: OSE Helper Bot <ose@lge.com>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/ui/accessibility/ax_node.cc
+++ src/ui/accessibility/ax_node.cc
@@ -778,7 +778,8 @@ bool AXNode::SetRoleMatchesItemRole(const AXNode* ordered_set) const {
       return item_role == ax::mojom::Role::kListItem ||
              item_role == ax::mojom::Role::kMenuItem ||
              item_role == ax::mojom::Role::kMenuItemRadio ||
-             item_role == ax::mojom::Role::kTreeItem;
+             item_role == ax::mojom::Role::kTreeItem ||
+             item_role == ax::mojom::Role::kCheckBox;
     case ax::mojom::Role::kMenu:
       return item_role == ax::mojom::Role::kMenuItem ||
              item_role == ax::mojom::Role::kMenuItemRadio ||
